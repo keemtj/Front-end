5d43e0eac113590fc5eef054782e5869083a1ee1
keemtj <keemgreat@gmail.com>
2020-08-27 04:28:19 +0900

    :white_check_mark: test code

--- src/Pages/MessagePage.js
+++ src/Pages/MessagePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import MessageWrapper from '../Components/Message/MessageWrapper';
-import MessageHeader from '../Components/Message/MessageHeader';
+import styled from 'styled-components';
+import { darken } from 'polished';
+import { Airbnb } from '@styled-icons/boxicons-logos/Airbnb';
 import MessageSubHeader from '../Components/Message/MessageSubHeader';
 import MessageMainWrapper from '../Components/Message/MessageMainWrapper';
 import ChatFlagModal from '../Components/Message/MessageModal';
@@ -10,16 +11,42 @@ import DetailSupportModal from '../Components/Message/DetailSupportModal';
 
 const MessagePage = () => {
   return (
-    <MessageWrapper>
-      <MessageHeader />
+    <MessagePageWrapper>
+      <MessageHeaderWrapper>
+        <Header>
+          <Airbnb size="4.5rem" />
+        </Header>
+      </MessageHeaderWrapper>
       <MessageSubHeader />
       <MessageMainWrapper />
       <ChatFlagModal />
       <DetailPdfModal />
       <DetailBusinessModal />
       <DetailSupportModal />
-    </MessageWrapper>
+    </MessagePageWrapper>
   );
 };
+
+const MessagePageWrapper = styled.div`
+  /* display: flex; */
+  position: absolute;
+`;
+
+const MessageHeaderWrapper = styled.div`
+  position: fixed;
+  z-index: 1;
+  background: ${({ theme }) => theme.color.white};
+  width: 100%;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid ${({ theme }) => darken(0.1, theme.color.lightGray)};
+  box-shadow: 1px 0 0.4rem ${({ theme }) => theme.color.shadow};
+  padding-left: 2rem;
+  color: ${({ theme }) => theme.color.main};
+  height: 8rem;
+`;
 
 export default MessagePage;
